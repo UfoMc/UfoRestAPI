c0a31494d57b05bff30474e7864213c36159e60a
UfoMc <99866680+UfoMc@users.noreply.github.com>
2024-04-13 17:54:29 +0200

    Update Main.java

--- main/java/de/matga/Main.java
+++ main/java/de/matga/Main.java
@@ -5,11 +5,7 @@ import de.matga.api.BootStrap;
 public class Main {
 
     public static void main(String[] args) {
-
-
         BootStrap bootStrap = new BootStrap();
         Runtime.getRuntime().addShutdownHook(new Thread(bootStrap::shutdown));
-
     }
-
 }
